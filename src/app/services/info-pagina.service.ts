3864cdaac23e8159fb1c2d9fe7aaeca9ab6171dc
Marco Huerta <marco.huerta.sc@itszapopan.edu.mx>
2019-06-28 13:33:52 -0500

    Fin sección 5 configuración de firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,20 +8,32 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
-    //console.log('Servicio de Info Página cargada');
+    this.cargarInfo();
+    this.cargarEquipo();
 
+  }
 
+  private cargarInfo(){
+     //console.log('Servicio de Info Página cargada');
     //leer el archivo JSON
-
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) =>{
-
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
-
         });
+  }
+
+
+  private cargarEquipo(){
+     //leer el archivo JSON
+     this.http.get('https://angular-html-7f3a1.firebaseio.com/equipo.json')
+     .subscribe((resp:any[]) =>{
+      this.equipo = resp;
+       console.log(resp);
+     });
+
   }
 }
