c9f6096f901f8459e20ac9c84d284ccca6dca888
Marco Huerta <marco.huerta.sc@itszapopan.edu.mx>
2019-07-01 11:29:57 -0500

    :wrench: Sección 8 pantalla de productos configuración de la búsqueda :construction:

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,7 @@ export class ProductosService {
    //bandera
   cargando = true;
   productos : Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
 
   constructor( private http: HttpClient) {
@@ -18,17 +21,70 @@ export class ProductosService {
    }
    private cargarProductos(){
      //url de mi proyecto de firebase
-     this.http.get('https://angular-html-7f3a1.firebaseio.com/productos_idx.json')
-        .subscribe((resp: Producto[])=>{
-          console.log(resp);
-          this.productos =  resp;
+     return new Promise((resolve,reject) => {
+      this.http.get('https://angular-html-7f3a1.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Producto[])=>{
+        this.productos =  resp;
 
 
-         setTimeout(() => {
-          this.cargando = false;
+       setTimeout(() => {
+        this.cargando = false;
+        resolve();
 
-         }, 2000);
+       }, 2000);
 
-        });
+      });
+
+     });
+
    }
+
+
+   getProducto(id: string){
+
+   return this.http.get(`https://angular-html-7f3a1.firebaseio.com/productos/${ id }.json`);
+
+   }
+
+   buscarProdcuto(termino: string){
+
+    if (this.productos.length === 0 ) {
+      //cargar productos
+      this.cargarProductos().then(()=>{
+
+        //ejecutar después de tener los productos
+        this.filtrarProductos(termino);
+      });
+
+    } else {
+      //aplicar el filtro si ya tengo productos
+      this.filtrarProductos(termino);
+
+    }
+
+
+
+   }
+
+   private filtrarProductos(termino: string){
+
+    //console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod =>{
+      const titulower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf(termino) >= 0 || titulower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+
+      }
+
+
+    });
+
+
+   }
+
+
+
 }
